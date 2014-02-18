fde5176966dbb423d385ff24096d4b15ff4ca812
Corey Kepple <coreyckepple@gmail.com>
2014-02-18 14:22:48 -0600

    made todolist.php

---
+++ public/todo_list.php
@@ -0,0 +1,34 @@
+<?php
+
+echo "<p>GET:</p>";
+var_dump($_GET);
+
+echo "<p>POST:</p>";
+var_dump($_POST);
+
+?>
+
+<html>
+<head>
+	<title>TODO List</title>
+</head>
+<body>
+	<h1>TODO list:</h1>
+	<ul>
+		<li>Wake Up</li>
+		<li>Eat Breakfast</li>
+		<li>Go to Codeup</li>
+		<li>Profit</li>
+	</ul>
+	<h2>Add items to list</h2>
+	<form method="GET" action="">
+		<p>
+		<label for="additem">Add Item to list:</label>
+			<input id="additem" name="additem" type="text" placeholder="Enter new TODO item">
+		</p>
+		<p>
+			<input type="submit">
+		</p>
+	</form>
+</body>
+</html>
