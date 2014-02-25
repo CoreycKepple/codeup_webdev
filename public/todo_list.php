711b75bb7834070c9a524be058caf95e85d62a90
Corey Kepple <coreyckepple@gmail.com>
2014-02-24 19:29:36 -0600

    finished todo_list.php ...... finally.

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,12 +1,33 @@
+<!DOCTYPE HTML!>
 <?php
+$filename = 'data/codeup_todo.txt';
+$handle = fopen($filename, 'r');
+$filesize = filesize($filename);
+if ($filesize != 0) {
+	$contents = fread($handle,$filesize );
+	fclose($handle);
+	$list = array();
+	$array = explode("\n", $contents);
+    foreach ($array as $value) {
+        array_push($list, $value);
+     }
+}else {
+	echo "<p> You have Nothing TODO </p>";
+	$list = array();
+}
 
-echo "<p>GET:</p>";
-var_dump($_GET);
-
-echo "<p>POST:</p>";
-var_dump($_POST);
-
+if (isset($_GET['remove'])) {
+	$key = $_GET['remove'];
+	unset($list[$key]);
+	$str = implode("\n", $list);
+	$handle = fopen($filename, 'w');
+	fwrite($handle, $str);
+	fclose($handle);
+	header("Location: todo_list.php");
+	exit(0);
+}
 ?>
+
 
 <html>
 <head>
@@ -14,21 +35,43 @@ var_dump($_POST);
 </head>
 <body>
 	<h1>TODO list:</h1>
-	<ul>
-		<li>Wake Up</li>
-		<li>Eat Breakfast</li>
-		<li>Go to CodeUp</li>
-		<li>Profit</li>
-	</ul>
+		<form method ="GET" action ="">
+			<ul>
+				<?php			
+				if(!empty($list)){
+				foreach ($list as $key => $item) {
+					echo "<li>{$item}
+					<a href='?remove={$key}' name='remove' id='remove'>Remove Item</a></li>";
+					}
+				}else {
+					echo "<h3>You have nothing to do? Find something :</h3>";
+					echo "<p><a href='http://google.com'> :-)</a></p>";
+				}
+				
+				?>
+			</ul>
+		</form>
 	<h2>Add items to list</h2>
-	<form method="GET" action="">
-		<p>
-		<label for="additem">Item to add:</label>
-			<input id="additem" name="additem" type="text" placeholder="Enter new TODO item">
-		</p>
-		<p>
-			<input type="submit">
-		</p>
-	</form>
+		<form method="POST" action="">
+			<p>
+			<label for="additem">Item to add:</label>
+				<input id="additem" name="additem" type="text" placeholder="Enter new TODO item">
+			</p>
+			<p>
+				<input type="submit">
+			</p>
+			<?php
+			if (!empty($_POST['additem'])) {
+				$new = implode($_POST, '');
+				array_push($list, $new);
+	            $string=implode("\n", $list);
+				$handle=fopen($filename, 'w');
+	            fwrite($handle, $string);
+	            fclose($handle);
+	            header("Location: todo_list.php");
+				}
+			?>
+			
+		</form>
 </body>
 </html>
