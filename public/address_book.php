a7bb8832e666ecb9c7367539687072adf09d0611
Corey Kepple <coreyckepple@gmail.com>
2014-02-26 15:00:31 -0600

    ben proofed my filed...for now.

--- public/address_book.php
+++ public/address_book.php
@@ -15,7 +15,6 @@ function newEntry($address_book,$filename){
 if (!empty($_POST)) {
 	if (empty($_POST['sendto'])) {
 		$error = 'You did not enter Recipients Name. Please enter missing information.';
-
 	}elseif (empty($_POST['address'])) {
 		$error = 'You did not enter Recipients Address. Please enter missing information.';
 	}elseif (empty($_POST['city'])) {
@@ -37,46 +36,52 @@ if (!empty($_POST)) {
 </head>
 <body>
 	<h2>Address Book Entry:</h2>
-		<ul>
+		<table>
+			<tr>
+				<th>Name:</th><th>Address:</th><th>City:</th><th>State:</th><th>Zip:</th><th>Phone (optional):</th>
 			<? if (empty($error)) : ?>
 				<? if (!empty($address_book)) : ?>
 						<? foreach ($address_book as $fields) : ?>
-							<? $fields = implode(' || ', $fields); ?>
-							<li><?= "$fields";?></li>
+						<tr>
+							<?php foreach ($fields as $field): ?>
+								<? $field = htmlspecialchars(strip_tags($field)); ?>
+								<td><?="|| $field ||";?></td>
+							<?php endforeach; ?>
+							</tr>
 						<? endforeach; ?>
 					<? else : ?>
-						<li><?= "Enter New Address" ?></li>
+						<tr><?= "Enter New Address" ?></tr>
 					<? endif; ?>
 			<? else : ?>
-				<li><?= $error; ?></li>
+				<tr><?= $error; ?></tr>
 			<? endif; ?> 
-		</ul>
+		</table>
 	<h2>Add New Address:</h2>
 	<hr>
 		<form method="POST" action="">
 			<p>
 				<label for="sendto">Recipients Name:</label>
-				<input id="sendto" name="sendto" type="text" value='<?= (empty($error)) ? "" : $_POST["sendto"];?>'>
+				<input id="sendto" name="sendto" type="text" value="<?= (empty($error)) ? '' : htmlspecialchars(strip_tags($_POST['sendto']));?>">
 			</p>
 			<p>
 				<label for="address">Recipients Address:</label>
-				<input id="address" name="address" type="text" value='<?=  (empty($error)) ? "" : $_POST["address"];?>'>
+				<input id="address" name="address" type="text" value="<?=  (empty($error)) ? '' : htmlspecialchars(strip_tags($_POST['address']));?>">
 			</p>
 			<p>
 				<label for="city">Recipients City:</label>
-				<input id="city" name="city" type="text" value='<?= (empty($error)) ? "" : $_POST["city"];?>'>
+				<input id="city" name="city" type="text" value="<?= (empty($error)) ? '' : htmlspecialchars(strip_tags($_POST['city']));?>">
 			</p>
 			<p>
 				<label for="state">Recipients State:</label>
-				<input id="state" name="state" type="text" value='<?= (empty($error)) ? "" : $_POST["state"];?>'>
+				<input id="state" name="state" type="text" value="<?= (empty($error)) ? '' : htmlspecialchars(strip_tags($_POST['state']));?>">
 			</p>
 			<p>
 				<label for="zip">Recipients Zip-Code:</label>
-				<input id="zip" name="zip" type="text" value='<?= (empty($error)) ? "" : $_POST["zip"];?>'>
+				<input id="zip" name="zip" type="text" value="<?= (empty($error)) ? '' : htmlspecialchars(strip_tags($_POST['zip']));?>">
 			</p>
 			<p>
 				<label for="phone">Recipients Phone Number:</label>
-				<input id="phone" name="phone" type="text" value='<?= (empty($error)) ? "" : $_POST["phone"];?>'>
+				<input id="phone" name="phone" type="text" value="<?= (empty($error)) ? '' : htmlspecialchars(strip_tags($_POST['phone']));?>">
 			<p>
 				<input type='submit' value='Add New Entry'>
 			</p>
