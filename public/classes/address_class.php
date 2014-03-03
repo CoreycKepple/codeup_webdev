706a606bec757f0ef1385267214828873312a1d2
Corey Kepple <coreyckepple@gmail.com>
2014-03-02 20:25:11 -0600

    minor update to __constructor

--- public/classes/address_class.php
+++ public/classes/address_class.php
@@ -5,6 +5,8 @@ require_once('filestore.php');
 class AddressDataStore extends Filestore {
 
 	function __constructor(){
+        $this->strtolower(filename);
+        parent:: __contructor();
 		
 	}
 
